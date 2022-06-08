aabc3db17675601e3a99c58a812316bc32bce196
qwer45us <agni.tripathi@kellogg.northwestern.edu>
2022-06-08 02:18:02 +0000

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get "/", :controller => "places", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
   resources "places"
   resources "posts"
   resources "sessions"
   resources "users"
+
 end
